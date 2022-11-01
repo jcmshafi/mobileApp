955ea54fb440d9a741f43331e433f40fc6624e61
Shafiujjaman-shafi <jcmshafi96@gmail.com>
2022-11-02 00:15:21 +0600

    2nd commit

--- app/src/main/java/com/example/mysimpleapp/MainActivity.java
+++ app/src/main/java/com/example/mysimpleapp/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int textview= 0;
+    private TextView tampil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +18,13 @@ public class MainActivity extends AppCompatActivity {
     }
     public void toast(View view){
         Context context=getApplicationContext();
-        Toast toast= Toast.makeText(context, R.string.message, Toast.LENGTH_SHORT);
+        Toast toast=Toast.makeText(context, R.string.message, Toast.LENGTH_SHORT);
         toast.show();
     }
     public void count(View view){
+        tampil=(TextView)findViewById(R.id.show_count);
+        textview++;
+        tampil.setText(Integer.toString(textview));
 
     }
 }
